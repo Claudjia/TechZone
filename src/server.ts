a1bdd27cf740b9f3e2a016203dc68d5d30587a1d
Claudia <solondraibeclaudia@gmail.com>
2026-09-24 14:55:19 +0300

    Add src/server.ts

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import { app } from './app';
+import { env } from './config/env';
+import { prisma } from './config/database';
+
+const server = app.listen(env.PORT, () => console.log(`API disponible sur http://localhost:${env.PORT}`));
+
+async function shutdown() {
+  server.close();
+  await prisma.$disconnect();
+  process.exit(0);
+}
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
